8a2abb229f5dd8abb67c7fb59766eef6d8dfba4b
Anthony Rossbach <anthonyrossbach@gmail.com>
2016-11-27 18:50:45 -0800

    Nested IF statement support and max function nested loops.

--- core/code_functions.php
+++ core/code_functions.php
@@ -1,10 +1,12 @@
 <?
 
 $ss_functions=array();
+$ss_functions_open=array();
 
 function ss_code_functions_register($t){
 	global $system;
 	global $ss_functions;
+	global $ss_functions_open;
 	
 	$n="";
 	$f="";
@@ -26,6 +28,7 @@ function ss_code_functions_register($t){
 				//--Reigster function now
 				if ($system["debug"]==true){ $system["debug_log"].="\r\n> Register Function End - End of function found, registered ".$name.""; }
 				$ss_functions["".$name.""]=removeblank($f);
+				$ss_functions_open["".$name.""]=0;
 				$f_reg_run=true;
 				$f_reg=false;
 				$name=false;
@@ -51,10 +54,19 @@ function ss_code_functions_register($t){
 
 function ss_code_function_run($func){
 	global $system;
+	global $settings;
 	global $ss_functions;
+	global $ss_functions_open;
+	
 	if (isset($ss_functions["".$func.""])){
-		if ($system["debug"]==true){ $system["debug_log"].="\r\n> Run Function F - ".$func.""; }
-		return ss_run_linebyline($ss_functions["".$func.""]);
+		if ($ss_functions_open["".$func.""]<=$settings["settings_function_loopmax"]){
+			if ($system["debug"]==true){ $system["debug_log"].="\r\n> Run Function F - ".$func.""; }
+			$ss_functions_open["".$func.""]++;
+			return ss_run_linebyline($ss_functions["".$func.""]);
+			$ss_functions_open["".$func.""]--;
+		}else{
+			if ($system["debug"]==true){ $system["debug_log"].="\r\n> Run Function F - ".$func." LIMIT HIT OF (".$settings["settings_function_loopmax"].")"; }
+		}
 	}else{
 		if ($system["debug"]==true){ $system["debug_log"].="\r\n> Failed To Run Function F - ".$func.""; }
 		return "";
--- core/run_linebyline.php
+++ core/run_linebyline.php
@@ -13,9 +13,11 @@ function ss_run_linebyline($t){
 	$v=array();
 	$v["function"]=false;
 	$v["ran"]=false;
-	$v["if"]=false; //--Turn true if in a IF statement
-	$v["if_disabled"]=false; //--Turn true if first part of statement is false so the code until end of IF does not run
 	$v["backquote"]=false;
+	$v["if_on"]=0;
+	$v["if_child"]=0; //--When we are running in a IF thats diabled and come across a IF, we mark it down so we know how many END's we must pass beore our own.
+	$v["if"]=array(); //--Turn true if in a IF statement
+	$v["if_disabled"]=array(); //--Turn true if first part of statement is false so the code until end of IF does not run
 	
 	$var=array();
 	
@@ -24,8 +26,13 @@ function ss_run_linebyline($t){
 		$l=ltrim($l);
 		if ($l!=""){
 			
+			if (!isset($v["if"][$v["if_on"]])){
+				$v["if"][$v["if_on"]]=false;
+				$v["if_disabled"][$v["if_on"]]=false;
+			}
+			
 			//--Backquote areas are auto return without processing
-			if (strpos($l, '`') !== false && $v["if_disabled"]==false){
+			if (strpos($l, '`') !== false && $v["if_disabled"][$v["if_on"]]==false){
 				$v["ran"]=true;
 				if ($v["backquote"]==false){
 					if ($system["debug"]==true){ $system["debug_log"].="\r\n> Backquote ON with backtrack to `"; } $v["backquote"]=true; $r.=substr($l, strpos($l, "`") + 1);
@@ -36,23 +43,35 @@ function ss_run_linebyline($t){
 				if ($v["backquote"]==true){ $v["ran"]=true; $r.=$l; }
 			}
 			
+			//--Find IF statements when already in blocked IF do child count
+			if ($v["backquote"]==false && $v["if_disabled"][$v["if_on"]]==true && $v["ran"]==false){
+				if (strpos(substr($l, 0, 6), 'if ') !== false){
+					$ifcheck=ss_run_linebyline_if($id,$l); //--Check if if statement to verify that it's real
+					if ($ifcheck!=false){
+						$v["if_child"]++;
+						$v["ran"]=true;
+					}
+				}
+			}
+			
 			//--Standard processing
-			if ($v["backquote"]==false && $v["if_disabled"]==false && $v["ran"]==false){
+			if ($v["backquote"]==false && $v["if_disabled"][$v["if_on"]]==false && $v["ran"]==false){
 				
 				//--Find IF statements
 				if (strpos(substr($l, 0, 6), 'if ') !== false){
-					$ifcheck=ss_run_linebyline_if($id,$l);
-					//--Check if if statement (I know)
+					$ifcheck=ss_run_linebyline_if($id,$l); //--Check if if statement (I know)
 					if ($ifcheck!=false && $v["ran"]==false){
 						if ($ifcheck=="yes"){
+							$v["if_on"]=$v["if_on"]+1;
 							$v["ran"]=true;
-							$v["if"]=true;
-							$v["if_disabled"]=false;
+							$v["if"][$v["if_on"]]=true;
+							$v["if_disabled"][$v["if_on"]]=false;
 						}
 						if ($ifcheck=="no"){
+							$v["if_on"]=$v["if_on"]+1;
 							$v["ran"]=true;
-							$v["if"]=true;
-							$v["if_disabled"]=true;
+							$v["if"][$v["if_on"]]=true;
+							$v["if_disabled"][$v["if_on"]]=true;
 						}
 					}
 				}
@@ -135,26 +154,36 @@ function ss_run_linebyline($t){
 			}
 			
 			//--IF statement ELSE
-			if (strpos($l, 'else') !== false && $v["backquote"]==false && $v["ran"]==false && $v["if"]==true){
-				if ($system["debug"]==true){ $system["debug_log"].="\r\n> LineByLine ELSE"; }
-				if ($v["if"]==true){
-					if ($v["if_disabled"]==true){
-						$v["if_disabled"]=false;
+			if (strpos($l, 'else') !== false && $v["backquote"]==false && $v["ran"]==false && $v["if"][$v["if_on"]]==true && $v["if_child"]==0){
+				if ($system["debug"]==true){ $system["debug_log"].="\r\n> LineByLine ELSE (".$v["if_on"].")"; }
+				if ($v["if"][$v["if_on"]]==true){
+					if ($v["if_disabled"][$v["if_on"]]==true){
+						$v["if_disabled"][$v["if_on"]]=false;
 					}else{
-						$v["if_disabled"]=true;
+						$v["if_disabled"][$v["if_on"]]=true;
 					}
 				}
 			}
 			
 			//--IF statement END
-			if (strpos($l, 'end') !== false && $v["backquote"]==false && $v["ran"]==false && $v["if"]==true){
-				if ($system["debug"]==true){ $system["debug_log"].="\r\n> LineByLine END"; }
-				if ($v["if"]==true){
-					$v["ran"]=false;
-					$v["if_disabled"]=false;
-					$v["if"]=false;
+			if (strpos($l, 'end') !== false && $v["backquote"]==false && $v["ran"]==false && $v["if"][$v["if_on"]]==true && $v["if_child"]==0){
+				if ($system["debug"]==true){ $system["debug_log"].="\r\n> LineByLine END (".$v["if_on"].")"; }
+				if ($v["if"][$v["if_on"]]==true){
+					$v["ran"]=true;
+					$v["if_disabled"][$v["if_on"]]=false;
+					$v["if"][$v["if_on"]]=false;
+					$v["if_on"]=$v["if_on"]-1;
 				}
 			}
+			
+			//--Find END statements when already in blocked IF do child count removal
+			if ($v["if_disabled"][$v["if_on"]]==true && $v["ran"]==false && $v["if_child"]>0){
+				if (strpos($l, 'end') !== false){
+					$v["if_child"]--;
+					$v["ran"]=true;
+				}
+			}
+			
 		}
 		$v["ran"]=false;
 	}
