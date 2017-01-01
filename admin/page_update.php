a2229ae14aa22d5a9d76d838c0fd13e838f6e185
Anthony Rossbach <anthonyrossbach@gmail.com>
2016-12-31 18:03:48 -0800

    Fix

--- admin/page_update.php
+++ admin/page_update.php
@@ -4,7 +4,7 @@ echo '<div class="form-header"><h1>Update</h1></div><div class="form-content">';
 echo '<div class="form-group">Let me check for updates first...</div>';
 
 //--Get current version from GITHUB
-$data=json_decode(utf8_encode(file_get_contents('https://raw.githubusercontent.com/codesimplescript/core/master/version.json')), true); //Fetch Config Data
+$data=json_decode(utf8_encode(file_get_contents('https://raw.githubusercontent.com/codesimplescript/core/master/data.json')), true); //Fetch Config Data
 
 echo '<div class="form-group">You are running version '.$system_data["version"].', and the curent version is '.$data["version"].'</div>';
 
